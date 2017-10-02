930b5952fa60f1f34cbbae9ef95b1aee91c78e4c
Otávio Guimarães <otaviovgsr@gmail.com>
2017-10-01 21:47:35 -0300

    add marker to hijack

--- script.lua
+++ script.lua
@@ -1,14 +1,16 @@
-createVehicle (477, 2484, -1668, 14)
+local hijack = createVehicle (477, 2484, -1668, 14)
+createBlipAttachedTo (hijack, 55)
 
 function enterVehicle (thePlayer, seat, jacked )
 	local myMarker = createMarker(2443,-1647,12,"cylinder",3)
+	createBlipAttachedTo (myMarker, 51)
+	outputChatBox("Entregue o veículo na marca indicada.")
 	function deliveryCar (hitElement, matchingDimension) 
 		destroyElement(hitElement)
+		destroyElement(myMarker)
 		givePlayerMoney (thePlayer, 22385)
 		outputChatBox("Você entregou o veículo roubado!")
-		destroyElement(myMarker)
 	end
   addEventHandler( "onMarkerHit", myMarker, deliveryCar )
 end
-addEventHandler ( "onVehicleEnter", getRootElement(), enterVehicle )
-
+addEventHandler ( "onVehicleEnter", hijack, enterVehicle )
